26cdf7bc1ded2fe4577fe36beb18f1ddd8feb2d7
Utsav Mehta <utsavmehta2002@yahoo.com>
2023-03-05 18:51:26 +0530

    some changes.

--- hospitalmanagement-master/hospitalmanagement/urls.py
+++ hospitalmanagement-master/hospitalmanagement/urls.py
@@ -93,6 +93,7 @@ urlpatterns += [
     path('doctor-view-patient/<int:patient_id>', views.doctor_view_patient, name='doctor-view-patient'),
     path('doctor-prescribe-meds/<int:patient_id>', views.doctor_prescribe_medicine,name='doctor-prescribe-meds'),
     path('doctor-prescribe-tests/<int:patient_id>', views.doctor_prescribe_test,name='doctor-prescribe-test'),
+    path('doctor-notifications', views.doctor_notification, name='doctor-notifications'),
 ]
 
 # --- FOR DataEntry Related URLs (by Pranav) ---
@@ -116,20 +117,4 @@ urlpatterns += [
 #     path('doctor-view-appointment', views.doctor_view_appointment_view,name='doctor-view-appointment'),
 #     path('doctor-delete-appointment',views.doctor_delete_appointment_view,name='doctor-delete-appointment'),
 #     path('delete-appointment/<int:pk>', views.delete_appointment_view,name='delete-appointment'),
-# ]
-
-
-
-
-# #---------FOR PATIENT RELATED URLS-------------------------------------
-# urlpatterns +=[
-
-#     path('patient-dashboard', views.patient_dashboard_view,name='patient-dashboard'),
-#     path('patient-appointment', views.patient_appointment_view,name='patient-appointment'),
-#     path('patient-book-appointment', views.patient_book_appointment_view,name='patient-book-appointment'),
-#     path('patient-view-appointment', views.patient_view_appointment_view,name='patient-view-appointment'),
-#     path('patient-view-doctor', views.patient_view_doctor_view,name='patient-view-doctor'),
-#     path('searchdoctor', views.search_doctor_view,name='searchdoctor'),
-#     path('patient-discharge', views.patient_discharge_view,name='patient-discharge'),
-
 # ]
